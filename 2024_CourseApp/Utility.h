6418ec2a4d30c71bb02de9615e17b0649f9eeebc
林佑龍(dargonlonglong) <4b3g0146@office.stust.edu.tw>
2024-12-05 13:36:34 +0800

    新增 Student 和 Teacher 類別及相關檔案
    在 `2024_CourseApp.vcxproj` 和 `2024_CourseApp.vcxproj.filters` 中，新增了 `Student.cpp`、`Teacher.cpp`、`Student.h`、`Teacher.h`、`Utility.h` 和 `Utillity.h` 到相應的 `<ItemGroup>` 節點中。
    
    在 `Student.cpp` 和 `Teacher.cpp` 中，分別新增了對應的 `#include` 語句。
    
    在 `Student.h` 中，新增了 `Student` 類別，繼承自 `Person`，並包含了 `studentId`、`department` 和 `className` 三個私有成員變數。
    
    在 `Teacher.h` 中，新增了 `Teacher` 類別，繼承自 `Person`。
    
    在 `Utility.h` 中，新增了 `Department` 和 `ClassName` 兩個 `enum class`。
    
    在 `Utillity.h` 中，新增了 `#pragma once`。

---
+++ 2024_CourseApp/Utility.h
@@ -0,0 +1,20 @@
+#pragma once
+using namespace std;
+
+enum class Department {
+	ComputerSceince,
+	ElectricalEnginnering,
+	ElectronicEngineering,
+	InformationManagement,
+};
+
+enum class ClassName {
+	_1A,
+	_1B,
+	_2A,
+	_2B,
+	_3A,
+	_3B,
+	_4A,
+	_4B,
+};
